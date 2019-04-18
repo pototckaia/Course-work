d9476dc01b1d813d2ec1113193b7c0390bb13829
Nastya <pototckaia@gmail.com>
2019-04-18 18:37:41 +1000

    Add mean 3 day

--- mix.py
+++ mix.py
@@ -76,3 +76,20 @@ def print_mean(y_t, y_tr, first, second):
     print("Mean absolute percentage error on train {} and test {}".format(mape_train, mape_test))
 
 
+def MASE(training_series, testing_series, prediction_series):
+    """
+    Computes the MEAN-ABSOLUTE SCALED ERROR forcast error for univariate time series prediction.
+
+    parameters:
+        training_series: the series used to train the model, 1d numpy array
+        testing_series: the test series to predict, 1d numpy array or float
+        prediction_series: the prediction of testing_series, 1d numpy array (same size as testing_series) or float
+        absolute: "squares" to use sum of squares and root the result, "absolute" to use absolute values.
+    
+    """
+    n = training_series.shape[0]
+    d = np.abs(np.diff(training_series)).sum()/(n-1)
+    
+    errors = np.abs(testing_series - prediction_series)
+    return errors.mean()/d
+
