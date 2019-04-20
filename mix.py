ebb642f73498966f2a473375c61c557bd9b4af18
Nastya <pototckaia@gmail.com>
2019-04-20 20:08:26 +1000

    Add amplitude predict in new attemp

--- mix.py
+++ mix.py
@@ -51,29 +51,30 @@ def xgb_mape(preds, dtrain):
    return ('mape', -np.mean(np.abs((labels - preds) / (labels+1))))
 
 def print_mean(y_t, y_tr, first, second):
-    mse_test = mean_squared_error(y_t[first], y_t[second])
-    mse_train = mean_squared_error(y_tr[first], y_tr[second])
+    pd.options.mode.chained_assignment = None
+    mse_test = mean_squared_error(y_t.loc[:, first], y_t.loc[:, second])
+    mse_train = mean_squared_error(y_tr.loc[:, first], y_tr.loc[:, second])
     print("Mean squared error on train {} and test {}".format(mse_train, mse_test))
     
-    mae_test = mean_absolute_error(y_t[first], y_t[second])
-    mae_train = mean_absolute_error(y_tr[first], y_tr[second])
-    print("Mean absolute error on train {} and test {}".format(mae_train, mae_test))
+    # mae_test = mean_absolute_error(y_t[first], y_t[second])
+    # mae_train = mean_absolute_error(y_tr[first], y_tr[second])
+    # print("Mean absolute error on train {} and test {}".format(mae_train, mae_test))
 
-    evs_test = explained_variance_score(y_true=y_t[first], y_pred=y_t[second])
-    evs_train = explained_variance_score(y_true=y_tr[first], y_pred=y_tr[second])
-    print("Explained variance score on train {} and test {}".format(evs_train, evs_test))
+    # evs_test = explained_variance_score(y_true=y_t[first], y_pred=y_t[second])
+    # evs_train = explained_variance_score(y_true=y_tr[first], y_pred=y_tr[second])
+    # print("Explained variance score on train {} and test {}".format(evs_train, evs_test))
 
-    r2_train = r2_score(y_true=y_tr[first], y_pred=y_tr[second])
-    r2_test = r2_score(y_true=y_t[first], y_pred=y_t[second])
-    print("Coefficient of determination on train {} and test {}".format(r2_train, r2_test))
+    # r2_train = r2_score(y_true=y_tr[first], y_pred=y_tr[second])
+    # r2_test = r2_score(y_true=y_t[first], y_pred=y_t[second])
+    # print("Coefficient of determination on train {} and test {}".format(r2_train, r2_test))
 
-    mear_train = median_absolute_error(y_true=y_tr[first], y_pred=y_tr[second])
-    mear_test = median_absolute_error(y_true=y_t[first], y_pred=y_t[second])
-    print("Median absolute error on train {} and test {}".format(mear_train, mear_test))
+    # mear_train = median_absolute_error(y_true=y_tr[first], y_pred=y_tr[second])
+    # mear_test = median_absolute_error(y_true=y_t[first], y_pred=y_t[second])
+    # print("Median absolute error on train {} and test {}".format(mear_train, mear_test))
 
-    mape_train = mean_absolute_percentage_error(y_true=y_tr[first], y_pred=y_tr[second])
-    mape_test = mean_absolute_percentage_error(y_true=y_t[first], y_pred=y_t[second])
-    print("Mean absolute percentage error on train {} and test {}".format(mape_train, mape_test))
+    # mape_train = mean_absolute_percentage_error(y_true=y_tr[first], y_pred=y_tr[second])
+    # mape_test = mean_absolute_percentage_error(y_true=y_t[first], y_pred=y_t[second])
+    # print("Mean absolute percentage error on train {} and test {}".format(mape_train, mape_test))
 
 
 def MASE(training_series, testing_series, prediction_series):
